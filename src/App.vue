d7dc8fd49dca8306a444894d006e7a236ea855bc
csk200387 <csk200387@gmail.com>
2026-05-25 01:51:31 +0900

    통계를 위한 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppShell from '@/components/layout/AppShell.vue'
+import { Analytics } from '@vercel/analytics/vue'
 </script>
 
 <template>
   <AppShell>
     <RouterView />
   </AppShell>
+  <Analytics />
 </template>
